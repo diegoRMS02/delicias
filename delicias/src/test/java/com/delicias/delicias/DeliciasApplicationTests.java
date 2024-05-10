9ed1009d02d28d92471336a545177993d869ea71
diegoRMS02 <72329249@certus.edu.pe>
2024-05-10 08:12:15 -0500

    Inicio de proyecto delicias

---
+++ delicias/src/test/java/com/delicias/delicias/DeliciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delicias.delicias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
